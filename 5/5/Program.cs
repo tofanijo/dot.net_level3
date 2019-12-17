9c3da4251eb9cc3c328ec74cc5c6808ab055e3cd
tofanijo <tofanijo@gmail.com>
2019-12-17 19:25:16 +0200

    nuima po kablelio

---
+++ 5/5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 1234.75555;
+            int a;
+
+            //a = x;
+
+            a = (int)x;
+
+            Console.WriteLine(x);
+            Console.WriteLine(a);
+
+            Console.ReadLine();
+        }
+    }
+}
