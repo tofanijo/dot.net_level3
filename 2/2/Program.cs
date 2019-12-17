9334c0768e71a8f19e3c7bc5a16bc009c6699e9c
tofanijo <tofanijo@gmail.com>
2019-12-17 18:39:29 +0200

    2

---
+++ 2/2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double skaicius1 = 0.1 * 10;
+            double skaicius2 = 0;
+
+            for (int ctr = 0; ctr < 10; ctr++)
+            {
+                skaicius2 += 0.1;
+            }
+
+            Console.WriteLine("{0:R} = {1:R} : {2}", skaicius1, skaicius2, skaicius1 == skaicius2);
+            Console.ReadLine();
+            }
+        }
+    }
+
