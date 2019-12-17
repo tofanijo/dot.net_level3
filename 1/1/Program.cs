708b57c5d55f1eb8f4b58e9122b7f3a7ff9da1f0
tofanijo <tofanijo@gmail.com>
2019-12-17 18:34:11 +0200

    double/float

---
+++ 1/1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float skaiciusFloat = 1f / 3;
+            double skaiciusDouble = 1d / 3;
+
+            if (skaiciusFloat == skaiciusDouble)
+            {
+                Console.WriteLine("skaiciusFloat {0} YRA skaiciusDouble { 1}", skaiciusFloat, skaiciusDouble);
+            }
+            else
+            {
+                Console.WriteLine("skaiciusFloat {0} NERA lygus skaiciusDouble {1}", skaiciusFloat, skaiciusDouble);
+               
+            }
+            Console.ReadLine();
+        }
+    }
+}
