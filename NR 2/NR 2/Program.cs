a267776f8b8664283c7761265d87a7cd68e78029
tofanijo <tofanijo@gmail.com>
2019-12-17 20:04:47 +0200

    Greicio skaiciuokle

---
+++ NR 2/NR 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NR_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite atstuma metrais");
+            double atstumas = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Iveskite laika sekundem");
+            double laikas = Convert.ToDouble(Console.ReadLine());
+
+            double km = atstumas / 1000;
+            double h = laikas / (60 * 60);
+
+            Console.WriteLine("km/h = {0}", km / h);
+            Console.ReadLine();
+
+
+        }
+    }
+}
