34d9d0357d78d4e2e84cefef08ba0169e634c2c5
tofanijo <tofanijo@gmail.com>
2019-12-17 19:47:03 +0200

    1 3simboliai atvirkst. tvarka

---
+++ NR 1/NR 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NR_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           char simbolis1;
+           char simbolis2;
+           char simbolis3;
+
+            Console.WriteLine("Iveskite pima simboli");
+            simbolis1 = Console.ReadKey().KeyChar;
+            Console.WriteLine("");
+
+            Console.WriteLine("Iveskite antra simboli");
+            simbolis2 = Console.ReadKey().KeyChar;
+            Console.WriteLine("");
+
+            Console.WriteLine("Iveskite trecia simboli");
+            simbolis3 = Console.ReadKey().KeyChar;
+            Console.WriteLine("");
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
