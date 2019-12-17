c359ad378d27888246db4ccde091f16b6cd64fbd
tofanijo <tofanijo@gmail.com>
2019-12-17 21:05:22 +0200

    klaidingas rasymas

---
+++ be figuriniu skliaustu, KAIP NEREIKIA DARYTI/be figuriniu skliaustu, KAIP NEREIKIA DARYTI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace be_figuriniu_skliaustu__KAIP_NEREIKIA_DARYTI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ivesite spalva:");
+                string spalva = Console.ReadLine();
+
+            if ((spalva == "geltona") || (spalva == "zalia") || (spalva == "raudona"))
+                Console.WriteLine("{0} - tai spalva is trispalves", spalva);
+
+            else
+                Console.WriteLine("{0} - spalva nepriklauso trispalvei", spalva);
+
+            Console.ReadLine();
+        }
+    }
+}
