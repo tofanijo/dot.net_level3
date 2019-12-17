49bf6dd37bed12e873bad6c14744ccc8e771e4d0
tofanijo <tofanijo@gmail.com>
2019-12-17 20:33:48 +0200

    Rutulio ploto skaiciuokle

---
+++ NR 3/NR 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NR_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite rutulio diametra:");
+            double rutulioDiametras = Convert.ToDouble(Console.ReadLine());
+            double rutuliospindulys = rutulioDiametras / 2;
+
+
+            double rutulioPlotas = 4 * Math.PI * Math.Sqrt(rutulioDiametras);
+            Console.WriteLine("Rutulio plotas: {0}", rutulioPlotas);
+            Console.ReadLine();
+
+        }
+    }
+}
