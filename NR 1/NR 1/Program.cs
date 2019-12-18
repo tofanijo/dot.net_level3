e714cd694434969063598d436859f730700e67b4
tofanijo <tofanijo@gmail.com>
2019-12-18 10:23:12 +0200

    3 SIMBOLIAI, ATVIRKSTINE TVARKA

--- NR 1/NR 1/Program.cs
+++ NR 1/NR 1/Program.cs
@@ -26,6 +26,8 @@ namespace NR_1
             simbolis3 = Console.ReadKey().KeyChar;
             Console.WriteLine("");
 
+            Console.WriteLine("Ivesti simboliai atvirkstine tvarka: {0} {1} {2}", simbolis3, simbolis2, simbolis1 );
+
             Console.ReadLine();
 
 
